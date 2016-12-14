1df470dff04c3af84ec0a0bcb80848b4c24dd889
jasonkingpurestone <jason.king@purestone.co.uk>
2016-12-14 16:11:33 +0000

    Nuget package for encryption relative path change

--- Gigya.Module/Properties/AssemblyInfo.cs
+++ Gigya.Module/Properties/AssemblyInfo.cs
@@ -38,6 +38,6 @@ using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
 [assembly: ControllerContainer]
