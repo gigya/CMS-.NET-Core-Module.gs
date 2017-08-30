1739d79971921b2c5c32eb48dc1d31033f458c8a
jasonkingpurestone <jason.king@purestone.co.uk>
2017-08-30 14:49:46 +0100

    Umbraco 7.6.5 site with new packages

---
+++ Gigya.Umbraco.Demo.v765/App_Plugins/Gigya/backoffice/gigyaTree/gigyaTree.resource.js
@@ -0,0 +1,12 @@
+﻿//adds the resource to umbraco.resources module:
+angular.module('umbraco.resources').factory('gigyaTreeResource', ['$q', '$http', function ($q, $http) {
+        return {
+            get: function (id) {
+                return $http.get("/umbraco/backoffice/Gigya/GigyaSettingsApi/Get/" + id);
+            },
+            save: function (data) {
+                return $http.post("/umbraco/backoffice/Gigya/GigyaSettingsApi/Save", data);
+            }
+        };
+    }]
+);
