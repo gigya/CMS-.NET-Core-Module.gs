d5f8a733aad61f4994411626fa02e8cb630bc71a
Jason King <jason.king@lewispurestone.com>
2018-08-15 14:52:31 +0100

    Remove demo sites

--- Umbraco/Gigya.Umbraco.Demo.v765/App_Plugins/GigyaDS/backoffice/gigyaDsTree/dashboard.controller.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module("umbraco").controller("Gigya.DS.DashboardController",
-	function ($scope, $routeParams, navigationService) {
-        // reload tree in case user has added a new homepage
-	    navigationService.syncTree({ tree: 'gigyaDsTree', path: [-2, -2], forceReload: true, activate: true });
-	});
--- Umbraco/Gigya.Umbraco.Demo/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gigya.Umbraco.Demo.Global" Language="C#" %>
